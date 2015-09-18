402e060d03813cf2c7f6dd6b8addddd51dd5bfb3
Richard <richard.lupton@gmail.com>
2015-09-18 22:27:15 +0100

    Added a layer generator, that will probably be removed, or at least changed massively, in the future.

--- main.hs
+++ main.hs
@@ -1,5 +1,16 @@
-data Neuron = Neuron Float [Float] deriving (Eq, Show)
+import Data.Array
+
+data Neuron = Neuron Float [Float] deriving (Eq, Show, Read)
+
+type Layer = [Neuron]
+
+startNode :: Int -> Neuron
+startNode n = Neuron 0 (replicate n 0)
 
 fire :: [Float] -> Neuron -> Float
 fire inputs (Neuron bias weights) = 1 / (1 + exp (-dot - bias))
 	where dot = sum $ zipWith (*) inputs weights
+
+nextLayer :: Layer -> Int -> Layer
+nextLayer nes n = replicate n (startNode m)
+                  where m = length nes
