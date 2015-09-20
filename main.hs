edf113be5574187ccd38a09bc400492ae9c8b889
Richard <richard.lupton@gmail.com>
2015-09-20 23:43:58 +0100

    Added some more basic functions ands structure. Need to decide on an effective structure for representing neural networks in Haskell. This needs to interact well with some kind of update function, and learning algorithm (taking grads etc).

--- main.hs
+++ main.hs
@@ -1,16 +1,26 @@
 import Data.Array
 
-data Neuron = Neuron Float [Float] deriving (Eq, Show, Read)
-
+type Neuron = (Float, [Float])
 type Layer = [Neuron]
 
+type Network = [Layer]
+
 startNode :: Int -> Neuron
-startNode n = Neuron 0 (replicate n 0)
+startNode n = (0, (replicate n 0))
 
 fire :: [Float] -> Neuron -> Float
-fire inputs (Neuron bias weights) = 1 / (1 + exp (-dot - bias))
+fire inputs (bias, weights) = 1 / (1 + exp (-dot-bias))
 	where dot = sum $ zipWith (*) inputs weights
 
 nextLayer :: Layer -> Int -> Layer
 nextLayer nes n = replicate n (startNode m)
                   where m = length nes
+
+step :: [Float] -> Layer -> [Float]
+step ins layer = map (fire ins) $ layer
+
+addLayer :: Layer -> Network -> Network
+addLayer l n = l:n
+
+runNetwork :: Network -> [Float] -> [Float]
+runNetwork net ins = foldr (flip step) ins net
